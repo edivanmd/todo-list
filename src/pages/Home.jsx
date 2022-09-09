5d8892e630fd91e30b175df7e83caccabf7cdf0c
edivanmd <edivanmd@gmail.com>
2022-09-09 16:43:31 +1000

    Create tasks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { AiFillPlusCircle } from 'react-icons/ai';
 import { db } from '../firebase';
-import { collection, onSnapshot, query, updateDoc, doc } from 'firebase/firestore';
+import { collection, onSnapshot, query, updateDoc, doc, addDoc } from 'firebase/firestore';
 import Task from './Task';
 import style from './styles';
 
 const Home = () => {
     const [tasks, setTasks] = useState([]);
-
+    const [input, setInput] = useState("");
 
     //Create task
+    const createTask = async (e) => {
+        e.preventDefault(e);
+        if(input === '') {
+            alert('Please, enter a valid task')
+            return
+        }
+        await addDoc(collection(db, 'tasks'), {
+            text:input,
+            completed: false,
+        });
+        setInput('');
+    }
 
     //Read task
     useEffect(() => {
@@ -36,11 +48,17 @@ const Home = () => {
     return (
         <div className={style.wrapHome}>
             <h3 className={style.heading}>My Todo List</h3>
-            <form className={style.form} action="">
-                <input className={style.input} type='text' placeholder='Add new task' />
+            <form className={style.form} onSubmit={createTask}>
+                <input 
+                    value={input} 
+                    onChange={(e) => setInput(e.target.value)} 
+                    className={style.input} 
+                    type='text' 
+                    placeholder='Add a new task' 
+                />
                 <button className={style.buttonAdd}><AiFillPlusCircle size={30} color="#fff" /></button>
             </form>
-            <p className={style.count}> You have got 3 tasks</p>
+            <p className={style.count}> You have got <span className={style.numberCount}>{`${tasks.length}`}</span> tasks</p>
             <ul className={style.gridContainer}>
                 {tasks.map((task, index) => (
                     <Task key={index} task={task} toggleComplete={toggleComplete} />
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,12 +1,13 @@
 const style = {
-    bg: 'h-screen w-screen p-4 bg-darker font',
-    wrapHome: 'container max-w-screen-xl container mx-auto',
+    bg: '',
+    wrapHome: 'container max-w-screen-xl container mx-auto my-9',
     gridContainer: 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8',
     heading: 'text-4xl font-fira font-bold text-light text-center p-5',
     form: 'flex justify-between my-6',
     input: 'border p-5 w-full text-xl text-center text-dark font-bold',
     buttonAdd: 'border p-4 ml2 bg-dark',
-    count: 'text-center p-5 text-2xl text-crimson',
+    count: 'text-center p-5 text-2xl text-light',
+    numberCount: 'text-crimson text-3xl font-bold',
 
     box: 'p-4 bg-dark rounded-md capitalize flex flex-col justify-between',
     boxCompleted: 'p-4 bg-blue rounded-md capitalize flex flex-col justify-between',
